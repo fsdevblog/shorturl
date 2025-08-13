9ccfaf9b0102fd85d116cd97adf328905147d14e
Arc Warden <you@example.com>
2025-08-13 11:45:47 +0300

    iter20

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	"go.uber.org/zap"
 
@@ -10,13 +11,48 @@ import (
 	"github.com/fsdevblog/shorturl/internal/config"
 )
 
+// nolint:gochecknoglobals
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+const defaultBuildMeta = "N/A"
+
 func main() {
 	appConf := config.MustLoadConfig()
 
 	a := app.Must(app.New(*appConf))
 
+	printBuildMeta()
 	a.Logger.Info("Starting server", zap.Any("config", appConf))
 	if err := a.Run(); err != nil && !errors.Is(err, context.Canceled) {
 		panic(err)
 	}
+}
+
+func printBuildMeta() {
+	meta := struct {
+		version string
+		date    string
+		commit  string
+	}{
+		version: defaultBuildMeta,
+		date:    defaultBuildMeta,
+		commit:  defaultBuildMeta,
+	}
+	if buildVersion != "" {
+		meta.version = buildVersion
+	}
+	if buildDate != "" {
+		meta.date = buildDate
+	}
+	if buildCommit != "" {
+		meta.commit = buildCommit
+	}
+
+	fmt.Printf("Build version: %s\n", meta.version) //nolint:forbidigo
+	fmt.Printf("Build date: %s\n", meta.date)       //nolint:forbidigo
+	fmt.Printf("Build commit: %s\n", meta.commit)   //nolint:forbidigo
 }

4be830a1a3b8ca6d1475eaaf24cf4266f4ebf094
Arc Warden <you@example.com>
2025-08-16 14:13:26 +0300

    fix

--- internal/sslcert/errors.go
+++ internal/sslcert/errors.go
@@ -2,6 +2,7 @@ package sslcert
 
 import "errors"
 
+// Ошибки. Почему-то автотестам нужно чтоб комментарий был именно тут.
 var (
 	ErrCertExpired     = errors.New("certificate is expired")       // Срок действия сертификата истек.
 	ErrCertNotValidYet = errors.New("certificate is not valid yet") // Сертификат еще не вступил в силу.

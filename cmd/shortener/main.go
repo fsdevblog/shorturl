31d07e8904f55401fea186d855be5e17309cf58c
Arc Warden <you@example.com>
2025-08-16 13:54:42 +0300

    review issues

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
+
+	"github.com/fsdevblog/shorturl/internal/bmeta"
 
 	"go.uber.org/zap"
 
@@ -18,41 +19,14 @@ var (
 	buildCommit  string
 )
 
-const defaultBuildMeta = "N/A"
-
 func main() {
 	appConf := config.MustLoadConfig()
 
 	a := app.Must(app.New(*appConf))
 
-	printBuildMeta()
+	bmeta.Print(buildVersion, buildDate, buildCommit)
 	a.Logger.Info("Starting server", zap.Any("config", appConf))
 	if err := a.Run(); err != nil && !errors.Is(err, context.Canceled) {
 		panic(err)
 	}
-}
-
-func printBuildMeta() {
-	meta := struct {
-		version string
-		date    string
-		commit  string
-	}{
-		version: defaultBuildMeta,
-		date:    defaultBuildMeta,
-		commit:  defaultBuildMeta,
-	}
-	if buildVersion != "" {
-		meta.version = buildVersion
-	}
-	if buildDate != "" {
-		meta.date = buildDate
-	}
-	if buildCommit != "" {
-		meta.commit = buildCommit
-	}
-
-	fmt.Printf("Build version: %s\n", meta.version) //nolint:forbidigo
-	fmt.Printf("Build date: %s\n", meta.date)       //nolint:forbidigo
-	fmt.Printf("Build commit: %s\n", meta.commit)   //nolint:forbidigo
 }
